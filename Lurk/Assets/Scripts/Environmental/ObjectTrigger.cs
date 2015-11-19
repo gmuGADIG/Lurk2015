ac3b86a083a46e42533a1bfb33498302bb1c158e
Ashley <ashley.crouch@rocketmail.com>
2015-11-19 14:15:09 -0500

    Missed a script

---
+++ Lurk/Assets/Scripts/Environmental/ObjectTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectTrigger : MonoBehaviour
+{
+    public GameObject platforms;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Need the Lightable script here
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Flammable lighter = col.gameObject.GetComponent<Flammable>();
+        if (lighter && lighter.lit == true)
+        {
+            platforms.SetActive(true);
+        }
+
+    }
+}
